ade528461686a8202515e62b3a678bab63991fe1
dimgraycat <dimgray.cat@gmail.com>
2015-11-30 10:59:32 +0900

    update config

--- PickUrl/Config.php
+++ PickUrl/Config.php
@@ -5,7 +5,7 @@ namespace PickUrl;
 class Config
 {
     const VERSION = '0.0.1';
-    const DATE_APPROVED = '2015-11-25';
+    const DATE_APPROVED = '2015-11-30';
 
     const REQUEST_GET   = "GET";
     const REQUEST_POST  = "POST";
